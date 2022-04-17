22b22785109ee99f12ec45a4fd8576f00fe86fad
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2022-04-17 07:38:13 -0400

    fixes for etumrep

---
+++ EtumrepMMO.Lib/GroupSeedFinder.cs
@@ -0,0 +1,82 @@
+﻿using PKHeX.Core;
+using PLARNGGui;
+using static EtumrepMMO.Lib.SpawnerType;
+
+namespace EtumrepMMO.Lib;
+
+public static class GroupSeedFinder
+{
+    public const byte MaxRolls = 32;
+
+    #region Seed Detection
+
+    /// <inheritdoc cref="FindSeed(IEnumerable{PKM},byte,SpawnerType)"/>
+    public static (ulong Seed, int FirstIndex) FindSeed(string folder, byte maxRolls = MaxRolls, SpawnerType mode = All)
+        => FindSeed(GetInputs(folder), maxRolls, mode);
+
+    /// <inheritdoc cref="FindSeed(IEnumerable{PKM},byte,SpawnerType)"/>
+    public static (ulong Seed, int FirstIndex) FindSeed(IEnumerable<string> files, byte maxRolls = MaxRolls, SpawnerType mode = All)
+        => FindSeed(GetInputs(files), maxRolls, mode);
+
+    /// <inheritdoc cref="FindSeed(IEnumerable{PKM},byte,SpawnerType)"/>
+    public static (ulong Seed, int FirstIndex) FindSeed(IEnumerable<byte[]> data, byte maxRolls = MaxRolls, SpawnerType mode = All)
+        => FindSeed(GetInputs(data), maxRolls, mode);
+
+    #endregion
+
+    #region Data Fetching
+
+    /// <summary> Gets entities from the provided input source. </summary>
+    public static IReadOnlyList<PKM> GetInputs(string folder) => GetInputs(Directory.EnumerateFiles(folder));
+
+    /// <inheritdoc cref="GetInputs(string)"/>
+    public static IReadOnlyList<PKM> GetInputs(IEnumerable<string> files) => GetInputs(files.Select(File.ReadAllBytes));
+
+    /// <inheritdoc cref="GetInputs(string)"/>
+    public static IReadOnlyList<PKM> GetInputs(IEnumerable<byte[]> data) => data.Select(PKMConverter.GetPKMfromBytes).OfType<PKM>().ToArray();
+
+    #endregion
+
+    /// <summary>
+    /// Returns all valid Group Seeds (should only be one) that generated the input data.
+    /// </summary>
+    /// <param name="data">Entities that were generated</param>
+    /// <param name="maxRolls">Max amount of PID re-rolls for shiny odds.</param>
+    /// <param name="mode">Group seed validation mode</param>
+    /// <returns>Default if no result found, otherwise a single seed (no duplicates are possible).</returns>
+    public static (ulong Seed, int FirstIndex) FindSeed(IEnumerable<PKM> data, byte maxRolls = MaxRolls, SpawnerType mode = All)
+    {
+        var entities = data.ToArray();
+        var ecs = Array.ConvertAll(entities, z => z.EncryptionConstant);
+
+        // Backwards we go! Reverse the pkm data -> seed first (this takes the longest, so we only do one at a time).
+        for (int i = 0; i < entities.Length; i++)
+        {
+            var entity = entities[i];
+            Program.main.etumrepdisplay.AppendText($"Checking entity {i+1}/{entities.Length} for group seeds...\n");
+            var pokeResult = RuntimeReversal.GetSeeds(entity, maxRolls);
+
+            foreach (var (pokeSeed, rolls) in pokeResult)
+            {
+                // Get seed for slot-pkm
+                var genSeeds = GenSeedReversal.FindPotentialGenSeeds(pokeSeed);
+                foreach (var genSeed in genSeeds)
+                {
+                    // Get the group seed - O(1) calc
+                    var groupSeed = GroupSeedReversal.GetGroupSeed(genSeed);
+                    if (mode.HasFlag(MultiSpawn) && GroupSeedValidator.IsMultiInitial(groupSeed, ecs, i))
+                        Program.main.etumrepdisplay.AppendText($"Found a multi-spawn group seed with PID roll count = {rolls}\n");
+                    else if (mode.HasFlag(SingleSpawn) && GroupSeedValidator.IsSingleSingle(groupSeed, ecs, i))
+                        Program.main.etumrepdisplay.AppendText($"Found a single-spawn group seed with PID roll count = {rolls}\n");
+                    else if (mode.HasFlag(MixedSpawn) && GroupSeedValidator.IsSingleMulti(groupSeed, ecs, i))
+                        Program.main.etumrepdisplay.AppendText($"Found a 1+{ecs.Length-1} spawn group seed with PID roll count = {rolls}\n");
+                    else
+                        continue;
+
+                    return (groupSeed, i);
+                }
+            }
+        }
+        return (default, -1);
+    }
+}
---
+++ EtumrepMMO.Lib/ReversalMethods/GenSeedReversal.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Z3;
+using PKHeX.Core;
+
+namespace EtumrepMMO.Lib;
+
+/// <summary>
+/// Reverses for middle step seeds by using Z3 to calculate.
+/// </summary>
+public static class GenSeedReversal
+{
+    private static readonly Context ctx = new(new Dictionary<string, string> { { "model", "true" } });
+
+    /// <summary>
+    /// Middle level seed calculation for the Generator Seed
+    /// </summary>
+    /// <param name="seed">Bottom level Entity seed.</param>
+    public static IEnumerable<ulong> FindPotentialGenSeeds(ulong seed)
+    {
+        var exp = CreateGenSeedModel(seed);
+
+        // Z3 is a theorem prover, and only proves if the model can be solved.
+        // To yield multiple possible values from the expression, we must re-evaluate with added constraints.
+        // Add each previously valid result as an "and this must not be a result".
+        // Yield until no results found.
+        while (Check(exp) is { } m)
+        {
+            foreach (var kvp in m.Consts) // should only be 1
+            {
+                var tmp = (BitVecNum)kvp.Value;
+                var possible = tmp.UInt64;
+                yield return possible;
+
+                // Force the model to ignore the above result for s0, so we may get a new result if any.
+                var constraint = ctx.MkNot(ctx.MkEq(GenSeedResult, tmp));
+                exp = ctx.MkAnd(exp, constraint);
+            }
+        }
+    }
+
+    private static readonly BitVecExpr GenSeedResult = ctx.MkBVConst("s0", 64);
+    private static readonly BitVecExpr GenSeedExpression = GetBaseGenSeedModel();
+
+    private static BoolExpr CreateGenSeedModel(ulong seed)
+    {
+        var real_seed = ctx.MkBV(seed, 64);
+        return ctx.MkEq(real_seed, GenSeedExpression);
+    }
+
+    private static BitVecExpr GetBaseGenSeedModel()
+    {
+        BitVecExpr s0 = GenSeedResult;
+        BitVecExpr s1 = ctx.MkBV(Xoroshiro128Plus.XOROSHIRO_CONST, 64);
+
+        // var slotRand = ctx.MkBVAdd(s0, s1);
+        s1 = ctx.MkBVXOR(s0, s1);
+        var tmp = ctx.MkBVRotateLeft(24, s0);
+        var tmp2 = ctx.MkBV(1 << 16, 64);
+        s0 = ctx.MkBVXOR(tmp, ctx.MkBVXOR(s1, ctx.MkBVMul(s1, tmp2)));
+        s1 = ctx.MkBVRotateLeft(37, s1);
+        return ctx.MkBVAdd(s0, s1); // genseed
+        // no rot/xor needed, the add result is enough.
+    }
+
+    private static Model? Check(BoolExpr cond)
+    {
+        Solver solver = ctx.MkSolver();
+        solver.Assert(cond);
+        Status q = solver.Check();
+        if (q != Status.SATISFIABLE)
+            return null;
+        return solver.Model;
+    }
+}
